d2f61d5ba8d781e1e51105a3c9444524c00ef88f
Akkey <84906515+Akkey57492@users.noreply.github.com>
2021-08-07 05:00:48 +0900

    Build#1

---
+++ installer.bat
@@ -0,0 +1,14 @@
+@echo off
+title libs installer
+cls
+pip install discord-components
+cls
+pip install discord
+cls
+pip install rcon
+cls
+echo Complete
+echo Press any key to exit
+pause>nul
+cls
+exit
